ae8fcca3c55660a3bf1013d5c6dea516f0d9ff3d
chenyun <1159807248@qq.com>
2017-06-12 16:20:22 +0800

    commit 增加支持 wcdb

--- Tabbar/app/src/main/java/cody/com/tabbar/MainActivity.java
+++ Tabbar/app/src/main/java/cody/com/tabbar/MainActivity.java
@@ -1,5 +1,6 @@
 package cody.com.tabbar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,19 +12,39 @@ import cody.com.tabbar.view.TabbarView;
 
 public class MainActivity extends AppCompatActivity {
     private TabbarView tabbarView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tabbarView = (TabbarView) findViewById(R.id.tabbar_view);
         List<TabItem> tabItems = new ArrayList<>();
-        TabItem tabItem = new TabItem("first0", null,null,null,null);
+        TabItem tabItem = new TabItem("toolbar", null, null, null, null);
         tabItem.setSelected(true);
         tabItems.add(tabItem);
-        tabItems.add(new TabItem("first1", null,null,null,null));
-        tabItems.add(new TabItem("first2", null,null,null,null));
-        tabItems.add(new TabItem("first3", null,null,null,null));
-        tabItems.add(new TabItem("first4", null,null,null,null));
+        tabItems.add(new TabItem("first1", null, null, null, null));
+        tabItems.add(new TabItem("first2", null, null, null, null));
+        tabItems.add(new TabItem("first3", null, null, null, null));
+        tabItems.add(new TabItem("first4", null, null, null, null));
+        tabItems.add(new TabItem("first5", null, null, null, null));
         tabbarView.setTabItems(tabItems);
+        tabbarView.setOnItemSelectListener(new TabbarView.onItemSelectListener() {
+            @Override
+            public void onItemSelected(int index) {
+                switch (index) {
+                    case 0:
+                        Intent intent = new Intent(MainActivity.this, ToolBarActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+
+                }
+            }
+        });
     }
 }
---
+++ Tabbar/app/src/main/java/cody/com/tabbar/ToolBarActivity.java
@@ -0,0 +1,36 @@
+package cody.com.tabbar;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class ToolBarActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tool_bar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        ActionBar actionBar = getSupportActionBar();
+//        if (actionBar != null) {
+//            actionBar.setDefaultDisplayHomeAsUpEnabled(true);
+//        }
+        navigationView.setCheckedItem(R.id.nav_call);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+}
